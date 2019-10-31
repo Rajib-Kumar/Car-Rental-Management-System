feb387be7bf65a2d5a5c79ba69ce4f9667e6daed
Rajib Kumar Paul <krajib60@gmail.com>
2019-10-31 16:36:34 +0600

    Add files via upload

---
+++ public/js/custom.js
@@ -0,0 +1,74 @@
+$(function(){
+	    $('.navbar').affix({
+	      offset: {
+	        top: $("header").outerHeight(true)}
+	    });
+	});
+
+
+jssor_1_slider_init = function() {
+
+    var jssor_1_SlideoTransitions = [
+      [{b:-1,d:1,o:-0.7}],
+      [{b:900,d:2000,x:-379,e:{x:7}}],
+      [{b:900,d:2000,x:-379,e:{x:7}}],
+      [{b:-1,d:1,o:-1,sX:2,sY:2},{b:0,d:900,x:-171,y:-341,o:1,sX:-2,sY:-2,e:{x:3,y:3,sX:3,sY:3}},{b:900,d:1600,x:-283,o:-1,e:{x:16}}]
+    ];
+
+    var jssor_1_options = {
+      $AutoPlay: 1,
+      $SlideDuration: 800,
+      $SlideEasing: $Jease$.$OutQuint,
+      $CaptionSliderOptions: {
+        $Class: $JssorCaptionSlideo$,
+        $Transitions: jssor_1_SlideoTransitions
+      },
+      $ArrowNavigatorOptions: {
+        $Class: $JssorArrowNavigator$
+      },
+      $BulletNavigatorOptions: {
+        $Class: $JssorBulletNavigator$
+      }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*#region responsive code begin*/
+
+    var MAX_WIDTH = 3000;
+
+    function ScaleSlider() {
+        var containerElement = jssor_1_slider.$Elmt.parentNode;
+        var containerWidth = containerElement.clientWidth;
+
+        if (containerWidth) {
+
+            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+            jssor_1_slider.$ScaleWidth(expectedWidth);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+
+    ScaleSlider();
+
+    $Jssor$.$AddEvent(window, "load", ScaleSlider);
+    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+    /*#endregion responsive code end*/
+};
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
